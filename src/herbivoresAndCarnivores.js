8ca39a67b6b9b880727019110cad08ea2d51dd4c
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-09-30 09:48:16 +0300

    Solution without toggling

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -16,11 +16,7 @@ class Herbivore extends Animal {
     Animal.alive.push(this);
   }
   hide() {
-    if (this.hidden === false) {
-      this.hidden = true;
-    } else {
-      this.hidden = !this.hidden;
-    }
+    this.hidden = true;
   }
 }
 
